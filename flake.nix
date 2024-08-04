bd8ddce8a70e65116f2e118098617180db513561
crertel <chris@kedagital.com>
2024-08-04 04:09:22 -0500

    Better nix flake.

--- flake.nix
+++ flake.nix
@@ -1,32 +1,48 @@
 {
   description = "ElixirRPG flake.";
 
-  inputs = {
-    nixpkgs = { url = "github:NixOS/nixpkgs/nixos-unstable"; };
-    flake-utils = { url = "github:numtide/flake-utils"; };
-  };
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        inherit (pkgs.lib) optional optionals;
-        pkgs = import nixpkgs { inherit system; };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-        elixir = pkgs.beam.packages.erlang.elixir;
-      in
-      with pkgs;
-      {
-        devShell = pkgs.mkShell {
-          buildInputs = [
-            elixir_1_17
-            elixir_ls
-            glibcLocales
-            pkg-config
-          ] ++ optional stdenv.isLinux inotify-tools
-          ++ optional stdenv.isDarwin terminal-notifier
-          ++ optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
-            CoreFoundation
-            CoreServices
-          ]);
-        };
-      });
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+
+      pkgs = import nixpkgs { inherit system; };
+      beamPkgs = with pkgs.beam_minimal; packagesWith interpreters.erlang_27;
+      erlang = beamPkgs.erlang;
+      elixir = beamPkgs.elixir_1_17;
+    in
+    {
+      devShells."${system}".default = pkgs.mkShell {
+        buildInputs = [
+          erlang
+          elixir
+
+          beamPkgs.hex
+          beamPkgs.elixir-ls
+
+          pkgs.inotifyTools
+          pkgs.nodejs
+        ];
+
+        ERL_INCLUDE_PATH = "${erlang}/lib/erlang/usr/include";
+        ERL_AFLAGS = "-kernel shell_history enabled";
+
+        shellHook = ''
+          # Allow mix to work on local directory
+          mkdir -p .nix-mix
+          mkdir -p .nix-hex
+          export MIX_HOME=$PWD/.nix-mix
+          export HEX_HOME=$PWD/.nix-hex
+          export ERL_LIBS=$HEX_HOME/lib/erlang/lib
+
+          # Concat paths
+          export PATH=$MIX_HOME/bin:$PATH
+          export PATH=$MIX_HOME/escripts:$PATH
+          export PATH=$HEX_HOME/bin:$PATH
+
+          # "Run: `mix archive.install hex phx_new` for phoenix."
+        '';
+      };
+    };
 }
