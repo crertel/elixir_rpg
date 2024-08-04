35534d7a82cfc3e2b455fa027b5d27da274d6070
crertel <chris@kedagital.com>
2024-08-04 04:21:18 -0500

    Add Github Actions for CI

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ElixirRpg.MixProject do
     [
       app: :elixir_rpg,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
