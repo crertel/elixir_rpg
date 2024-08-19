cc84cc9fef024efd30fb5f87208d00abbc0e48ae
crertel <chris@kedagital.com>
2024-08-18 22:49:27 -0500

    landing: fix failing test.

--- test/elixir_rpg_web/controllers/page_controller_test.exs
+++ test/elixir_rpg_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule ElixirRpgWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Eftsithe"
   end
 end
