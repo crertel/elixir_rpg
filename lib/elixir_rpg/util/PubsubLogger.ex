3315d85b0cb7504219646749957049de27286a55
crertel <chris@kedagital.com>
2024-08-24 03:19:06 -0500

    util: use proper inspiration from console backend for pubsub logge.

--- lib/elixir_rpg/util/PubsubLogger.ex
+++ lib/elixir_rpg/util/PubsubLogger.ex
@@ -1,34 +1,140 @@
 defmodule ElixirRpg.PubSubLoggerBackend do
+  @moduledoc ~S"""
+  A logger backend that logs messages by printing them to the console.
+
+  Adapted shamelessly from the console logger.
+
+  ## Options
+
+    * `:exchange` - the exchange to publish to
+
+    * `:topic` - the topic to publish to
+
+    * `:level` - the level to be logged by this backend.
+      Note that messages are filtered by the general
+      `:level` configuration for the `:logger` application first.
+
+    * `:format` - the format message used to print logs.
+      Defaults to: `"\n$time $metadata[$level] $message\n"`.
+      It may also be a `{module, function}` tuple that is invoked
+      with the log level, the message, the current timestamp and
+      the metadata and must return `t:IO.chardata/0`. See
+      `Logger.Formatter`.
+
+    * `:metadata` - the metadata to be printed by `$metadata`.
+      Defaults to an empty list (no metadata).
+      Setting `:metadata` to `:all` prints all metadata. See
+      the "Metadata" section in the `Logger` documentation for
+      more information.
+
+  Here is an example of how to configure this backend in a
+  `config/config.exs` file:
+
+      config :logger, PubSubLoggerBackend,
+        format: "\n$time $metadata[$level] $message\n",
+        metadata: [:user_id]
+
+  """
+
   @behaviour :gen_event
 
-  def init(__MODULE__) do
-    {:ok, %{}}
+  defstruct topic: nil,
+            exchange: nil,
+            format: nil,
+            level: nil,
+            metadata: nil
+
+  @impl true
+  def init(atom) when is_atom(atom) do
+    config = read_env()
+    {:ok, init(config, %__MODULE__{})}
   end
 
-  def handle_call({:configure, _options}, state) do
-    {:ok, :ok, state}
+  def init({__MODULE__, opts}) when is_list(opts) do
+    config = configure_merge(read_env(), opts)
+    {:ok, init(config, %__MODULE__{})}
   end
 
-  def handle_event({level, _gl, {Logger, msg, timestamp, metadata}}, state) do
-    message = format_message(level, msg, timestamp, metadata)
-    Phoenix.PubSub.broadcast(ElixirRpg.PubSub, "util:log:messages", {:log, message, format_timestamp(timestamp)})
+  @impl true
+  def handle_call({:configure, options}, state) do
+    {:ok, :ok, configure(options, state)}
+  end
+
+  @impl true
+  def handle_event(
+        {level, _gl, {Logger, msg, ts, md}},
+        %{format: format, metadata: keys, level: log_level, topic: topic, exchange: exchange} =
+          state
+      ) do
+    {:erl_level, level} = List.keyfind(md, :erl_level, 0, {:erl_level, level})
+
+    if meet_level?(level, log_level) do
+      formatted_msg = Logger.Formatter.format(format, level, msg, ts, take_metadata(md, keys))
+
+      Phoenix.PubSub.broadcast(
+        exchange,
+        topic,
+        {Logger, level, formatted_msg}
+      )
+    end
+
     {:ok, state}
   end
 
-  def handle_event(:flush, state) do
-    {:ok, state}
+  def handle_event(_, state), do: {:ok, state}
+
+  @impl true
+  def handle_info(_, state), do: {:ok, state}
+
+  @impl true
+  def code_change(_old_vsn, state, _extra), do: {:ok, state}
+
+  @impl true
+  def terminate(_reason, _state), do: :ok
+
+  ## Helpers
+
+  defp meet_level?(_lvl, nil), do: true
+
+  defp meet_level?(lvl, min), do: Logger.compare_levels(lvl, min) != :lt
+
+  defp configure(options, state) do
+    config = configure_merge(read_env(), options)
+    Application.put_env(:logger, __MODULE__, config)
+    init(config, state)
   end
 
-  def handle_info(_, state) do
-    {:ok, state}
+  defp init(config, state),
+    do: %{
+      state
+      | format: Logger.Formatter.compile(Keyword.get(config, :format)),
+        topic: Keyword.get(config, :topic, "util:log:messages"),
+        exchange: Keyword.get(config, :exchange),
+        metadata: Keyword.get(config, :metadata, []) |> configure_metadata(),
+        level: Keyword.get(config, :level)
+    }
+
+  defp configure_metadata(:all), do: :all
+  defp configure_metadata(metadata), do: Enum.reverse(metadata)
+
+  defp configure_merge(env, options) do
+    Keyword.merge(env, options, fn
+      :colors, v1, v2 -> Keyword.merge(v1, v2)
+      _, _v1, v2 -> v2
+    end)
   end
 
-  defp format_message(level, msg, timestamp, _metadata) do
-    "[#{format_timestamp(timestamp)}] [#{level}] #{msg}"
+  defp take_metadata(metadata, :all), do: metadata
+
+  defp take_metadata(metadata, keys) do
+    Enum.reduce(keys, [], fn key, acc ->
+      case Keyword.fetch(metadata, key) do
+        {:ok, val} -> [{key, val} | acc]
+        :error -> acc
+      end
+    end)
   end
 
-  defp format_timestamp( {date, {hour, minute, second, millisecond}} ), do: :io_lib.format(
-    "~4..0B-~2..0B-~2..0B ~2..0B:~2..0B:~2..0B.~3..0B",
-    Tuple.to_list(date) ++ [hour, minute, second, millisecond]
-  ) |> IO.iodata_to_binary()
+  defp read_env,
+    do: Application.get_env(:logger, __MODULE__, Application.get_env(:logger, :console, []))
 end
--- lib/elixir_rpg_web/components/log_overlay_component.ex
+++ lib/elixir_rpg_web/components/log_overlay_component.ex
@@ -1,24 +1,30 @@
-defmodule ElixirRpgWeb.LogExpandoComponent do
+defmodule ElixirRpgWeb.LogOverlayComponent do
   use ElixirRpgWeb, :live_component
+
   def render(assigns) do
     ~H"""
-     <div class="fixed bottom-4 right-4">
-      <details class="bg-white shadow-lg rounded-lg overflow-hidden">
-        <summary class="bg-gray-800 text-white p-2 cursor-pointer">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline-block mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
-          </svg>
-          Logs
-        </summary>
-        <div class="p-4 max-h-96 overflow-y-auto">
-          <ul id="log-messages" phx-update="stream" class="text-sm">
-            <li :for={{dom_id, message} <- @streams.log_messages} id={dom_id} class="mb-1 pb-1 border-b border-gray-200">
-              <%= message.message %>
-            </li>
-          </ul>
-        </div>
-      </details>
+    <div class="fixed inset-0 flex items-center justify-center pointer-events-none">
+      <div :if={@show} class="bg-black bg-opacity-50 p-4 rounded-lg w-2/3 h-1/2 overflow-y-auto">
+        <ul id="log-messages" phx-update="stream" class="text-white">
+          <li
+            :for={{dom_id, message} <- @streams.log_messages}
+            id={dom_id}
+            style={
+              case message.level do
+                :emergency -> "color: #F33;"
+                :error -> "color: #C11;"
+                :warning -> "color: yellow;"
+                :info -> "color: #FFF;"
+                :debug -> "color: brown;"
+                _ -> "color: #009;"
+              end
+            }
+          >
+            <%= message.message %>
+          </li>
+        </ul>
+      </div>
     </div>
-  """
+    """
   end
 end
