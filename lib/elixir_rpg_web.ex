1e678f895d44d0dad4f9bd0ad37e544e75998107
crertel <chris@kedagital.com>
2024-08-24 23:59:14 -0500

    assets: add textures folder

--- lib/elixir_rpg_web.ex
+++ lib/elixir_rpg_web.ex
@@ -17,7 +17,7 @@ defmodule ElixirRpgWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images art favicon.ico robots.txt)
+  def static_paths, do: ~w(assets fonts images art textures favicon.ico robots.txt)
 
   def router do
     quote do
