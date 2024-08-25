862bfc59394a4fb95448fd63841d71d012d6870c
crertel <chris@kedagital.com>
2024-08-25 02:38:18 -0500

    mix: include observer app

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule ElixirRpg.MixProject do
   def application do
     [
       mod: {ElixirRpg.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :observer]
     ]
   end
 
