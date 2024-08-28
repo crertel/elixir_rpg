aba2812653c01e47c578c9df0cfc3fbe7ba8e1bd
Chris <chris@chromaticbear.com>
2024-08-28 02:13:05 -0400

    util: add deg2rad, rad2deg

---
+++ lib/elixir_rpg/util/math.ex
@@ -0,0 +1,9 @@
+defmodule ElixirRpg.Util.Math do
+
+  @compile {:deg2rad, myfun: 1}
+  def deg2rad( degrees ), do: degrees * :math.pi() / 180.0
+
+  @compile {:rad2deg, myfun: 1}
+  def rad2deg( radians), do: radians * 180.0 / :math.pi
+
+end
